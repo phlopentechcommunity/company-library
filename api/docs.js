057cace4e33a0b5cfbaeb30bb4bbb09ac4e11bbf
Carl Angelo Nievarez <carlangelopamparonievarez@gmail.com>
2026-07-25 03:29:37 +0800

    Build Vercel company library app

---
+++ api/docs.js
@@ -0,0 +1,17 @@
+module.exports = (_request, response) => {
+  response.setHeader('content-type', 'text/html; charset=utf-8');
+  response.status(200).send(`<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Company Library API Docs</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script>SwaggerUIBundle({ url: '/api/openapi', dom_id: '#swagger-ui' });</script>
+</body>
+</html>`);
+};
